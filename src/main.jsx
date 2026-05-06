27bf9a18943aca0feec6cebba465f60f05c2a1e0
arthurfloripaaac-cell <arthurfloripa.aac@gmail.com>
2026-05-06 18:44:48 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from '/src/integra_unified.jsx'
+import App from './integra_unified.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
